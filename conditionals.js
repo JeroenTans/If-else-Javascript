bdf662a5ea5ad986f9cfa7a69f78c11a956fa3f4
JeroenTans <jeroentans1@gmail.com>
2021-03-28 13:06:13 +0200

    eerste keer javascript

--- conditionals.js
+++ conditionals.js
@@ -5,27 +5,41 @@
 const equalNumbers = 2 === 2;
 console.log('Is 2 gelijk aan 2?', equalNumbers);
 
+
+
 // ==========================================
 // 1. Declareer een variabele en sla daar de uitkomst van de vergelijking "Is 10 groter dan 20?" in op        // geeft false wanneer gelogd
 // ==========================================
-
+const biggerThen = 10 > 20;
+console.log('Is 10 groter dan 20?', biggerThen);
 
 // ==========================================
 // 2. Declareer een variabele en sla daar de uitkomst van de vergelijking "Is 10 ongelijk aan 11?" in op        // geeft true wanneer gelogd
 // ==========================================
-
+const notTheSameNumber = 10 !== 11;
+console.log('is 10 ongelijk aan 11? ', notTheSameNumber)
 
 // ==========================================
 // 3. Declareer een variabele en sla daar in op of de woorden 'zoet' en 'sappig' hetzelfde zijn                // geeft false wanneer gelogd
 // ==========================================
-
+const notTheSameString = 'zoet' !== 'sappig';
+console.log('Betekenen zoet en sappig hetzelfde? ', notTheSameString);
 
 // ==========================================
 // 4a. Declareer een variabele en sla daar het rapportcijfer 9 in op.
 // 4b. schrijf een script dat checkt of het rapportcijfer groter is dan 8.
 //     Als dat zo is, loggen we 'Cum laude!' in de console.                                                     // geeft 'Cum laude!' wanneer gelogd
 // ==========================================
-
+const grade = 9;
+if (grade >= 8) {
+    console.log("Cum laude!");
+}
+if (grade >= 5.5 && grade < 8) {
+    console.log("Goed gedaan maar kan beter!")
+}
+if (grade < 5.5) {
+    console.log("Helaas, volgende keer harder werken!")
+}
 
 // ==========================================
 // 5a. Gebruik onderstaande variabelen isRaining en temperature;
@@ -34,5 +48,12 @@ console.log('Is 2 gelijk aan 2?', equalNumbers);
 // Tip: verander de waarde van isRaining en temperature even handmatig om te kijken of jouw 2e conditie klopt
 // ==========================================
 
-const isRaining = false;
-const temperature = 23;
+const isRaining = true;
+const temperature = 11;
+
+if (isRaining === false && temperature > 21) {
+    console.log("Je kunt een korte broek aan!")
+}
+if (isRaining === true || temperature < 12) {
+    console.log("Doe een jas aan!");
+}
